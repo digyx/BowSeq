3a8aafc200a0c464aac377f62af5479826bcebc6
Devon Tingley <dtingley@twilit.io>
2020-10-19 14:54:22 -0400

    Added cli arguments to replace need to manually edit the code to generate different sequences

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # Bow Sequence Generator
 
 ## Getting Started
-1. Install Rust
-2. Install Python
-3. Clone the repo
-4. Run `cargo run` in the root of the repo
+1. Install Python
+2. Clone the repo
+3. Run the binary in Command Prompt (Windows) or the terminal (MacOS/Linux) with the proper arguments
+    1. Windows Example:  `BowSeq.exe 0 1`
+    2. MacOs/Linux Example: `./BowSeq 0 1`
+
+## Bash Style Documentation and Arguments
+```
+Bow Sequence Generator 0.1.0
+
+Usage:  BowSeq alpha_term beta_term [options]
+
+Options:
+    -count <count>  Amount of rows you would like generated
+    -type <string>  Representation of the numbers in the sequence (float, alphabeta)
+    -minmax         Find the maximum and minimum values in the sequence
+    -find <float>   Return the indexes where a specific value appears
+    -rowFormat      Log the sequence with row formatting in rows.txt
+```
 
 ## RAM Usage
 Since the amount of terms generated is doubled for each additional row, a small amount of rows leads a large amount of RAM being used.  Here are some examples of how much memory is used for a specified amount of rows:
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use std::env;
 use std::vec::Vec;
 use std::fs::File;
 use std::io::prelude::*;
-use std::process::Command;
+use std::process::{Command, exit};
 
 mod sequence;
 use sequence::Sequence;
@@ -16,14 +17,25 @@ use sequence::AlphaBeta;
     30 Rows : 32 GB 
 */
 
+struct SequenceParams {
+    alpha: f32,
+    beta: f32,
+    row_count: u32,
+    sequence_type: String,
+    gen_rows: bool,
+    min_max: bool,
+    find_elem: f32,
+}
+
+
 fn main() {
-    let alpha = 0.0;
-    let beta = 1.0;
-    let row_count = 10;
-    let num_to_find = 7.0;
+    let seq_params = get_sequence_params();
+
+    let alpha = seq_params.alpha;
+    let beta = seq_params.beta;
+    let row_count = seq_params.row_count;
     
-    let seq_type = "float";
-    let format_as_rows = true;
+    let seq_type = seq_params.sequence_type.as_str();
 
     let base = match seq_type {
         "float" => {
@@ -50,11 +62,66 @@ fn main() {
     let s = sequence_generator(row_count, base);
     let s_float = s.clone().float();
 
-    min_max(s_float.clone());
-    find_elem_index(s_float.clone(), num_to_find);
+    // Optional functions
+    if seq_params.gen_rows {
+        row_generator(s.clone());
+    }
+    
+    if seq_params.min_max {
+        min_max(s_float.clone());
+    }
+    
+    if seq_params.find_elem != 0.0 {
+        find_elem_index(s_float.clone(), seq_params.find_elem);
+    }
+}
 
-    if format_as_rows {
-        row_generator(s);
+fn get_sequence_params() -> SequenceParams{
+    let mut args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        println!("error:  Must supply at least one alpha and beta value");
+        exit(1);
+    }
+    
+    let alpha: f32 = args.remove(1).parse().unwrap();
+    let beta: f32 = args.remove(1).parse().unwrap();
+
+    let mut row_count: u32 = 10;
+    let mut seq_type = String::from("float");
+    let mut gen_rows: bool = false;
+    let mut min_max: bool = false;
+    let mut find_elem: f32 = 0.0;
+
+    for i in 1..args.len() {
+        match args[i].as_str() {
+            "-count" => {
+                row_count = args[i+1].parse().unwrap();
+            },
+            "-type" => {
+                seq_type = args[i+1].clone();
+            },
+            "-rowFormat" => {
+                gen_rows = true;
+            },
+            "-minmax" => {
+                min_max = true;
+            },
+            "-find" => {
+                find_elem = args[i+1].parse().unwrap();
+            }
+            _ => {}
+        }
+    }
+    
+    SequenceParams{
+        alpha: alpha,
+        beta: beta,
+        row_count: row_count,
+        sequence_type: seq_type,
+        gen_rows: gen_rows,
+        min_max: min_max,
+        find_elem: find_elem,
     }
 }
 
@@ -87,8 +154,9 @@ fn min_max(s: Vec<f32>) {
         }
     }
 
-    println!("Max: {}", max);
-    println!("Min: {}", min);
+    println!("Minimum and Maximum:");
+    println!("\tMax: {}", max);
+    println!("\tMin: {}", min);
 }
 
 fn find_elem_index(s: Vec<f32>, n: f32) {
@@ -97,14 +165,20 @@ fn find_elem_index(s: Vec<f32>, n: f32) {
         if *elem == n {results.push(index);}
     }
 
+    let mut i = 0;
     println!("Indexes where {} occurs:", n);
-    for i in results {
-        println!("\t{}", i);
+    for result in results {
+        if i == 5 {
+            println!("");
+            i = 0;
+        }
+        print!("\t{}", result);
+        i += 1;
     }
 }
 
 fn row_generator(mut s: Sequence) {
-    let mut file = File::create("target/sequence.txt").unwrap();
+    let mut file = File::create("sequence.txt").unwrap();
     let mut contents = format!("{}", s.remove(0));
     
     println!("Formatting data...");
@@ -116,6 +190,6 @@ fn row_generator(mut s: Sequence) {
     file.write_all(contents.as_bytes()).unwrap();
 
     Command::new("python3")
-        .args(&["src/rows.py", "target/sequence.txt", "target/rows.txt"])
+        .args(&["src/rows.py", "sequence.txt", "rows.txt"])
         .status().unwrap();
 }
