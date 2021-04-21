131b0b732c5621b8b35e9f3f39bcf25297534fdd
Devon Tingley <dtingley@twilit.io>
2021-04-20 20:40:12 -0400

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -27,15 +27,21 @@ fn main() {
     
     let seq_type = seq_params.sequence_type.as_str();
 
+    // Initialize the Sequnce acording to the definition
     let mut base: Sequence = Vec::new();
     base.push(AlphaBeta{alpha: 1, beta: 0});
     base.push(AlphaBeta{alpha: 0, beta: 1});
 
+    /*
+        Slower method of calculating large rows, but uses almost no memory.
+        In the future, this can be used after a certain row count, allowing for calculating
+        more rows where the current method would be impractical because of RAM limitations
+    */
     if seq_params.standalone {
         let base: i64 = 2;
-        let size = (base.pow(row_count + 1) - 1 )as u32;
+        let length = (base.pow(row_count + 1) - 1 )as u32;
     
-        let seq = Box::new(sequence::new_standalone(alpha, beta, size));
+        let seq = Box::new(sequence::new_standalone(alpha, beta, length));
 
         if seq_params.sum {
             println!("\nSum:");
@@ -60,18 +66,27 @@ fn main() {
         return
     }
 
+    /*
+        Only used for generating a Row Format txt file
+        AlphaBeta notation has no other practical use as of writing this comment
+    */
     if seq_type == "alphabeta" {
         let s = sequence_generator(row_count, base);
         row_generator(s);
         return
     }
 
+    /* 
+        The sequence is generated and stored in a box.
+        Boxes function as storing on the Heap rather than the Stack, so the object is
+        not passed around when used by a function.  This halves RAM usage by allowing
+        us to use the sequence without cloning the sequence every time it needs to be used
+    */
     println!("Generating sequence...");
-    let s = sequence_generator(row_count, base);
-    let seq = Box::new(s);
+    let seq = Box::new(sequence_generator(row_count, base));
     println!("Done generating.");
 
-    // Optional functions
+    // Analysis functions
     if seq_params.find_elem != 0.0 {
         find_elem_index(&seq, seq_params.find_elem, alpha, beta);
     }
@@ -96,23 +111,39 @@ fn main() {
     }
 }
 
-fn sequence_generator(row_count: u32, mut s: Vec<AlphaBeta>) -> Vec<AlphaBeta> {
+/*
+    The sequence is generated using the recursive definition and stored in an AlphaBeta
+    Vector, renamed sequence for convenience sake.  While very fast for computing values,
+    this uses a collosal amount of RAM as each row calculated doubles the amount used.
+
+    Using the above standlone method, analysis computations could be done with
+    higher row amounts by using on-the-fly computation.  Combining the two, as well as
+    parallelising the number generations would add significant value.
+*/
+fn sequence_generator(row_count: u32, mut s: Sequence) -> Sequence {
     let base: i64 = 2;
     let length = base.pow(row_count + 1) - 1;
 
     for num in 1..(length-1) {
+        // The current index is even
         if num % 2 == 0 {
-            let add = s[(num / 2) as usize] + s[(num / 2 + 1) as usize];
-            s.push(add);
-        } else {
-            let add = s[((num - 1) / 2) as usize];
-            s.push(add);
+            let cur_term = s[(num / 2) as usize] + s[(num / 2 + 1) as usize];
+            s.push(cur_term);
+            continue
         }
+        // The current index is odd
+        let cur_term = s[((num - 1) / 2) as usize];
+        s.push(cur_term);
     };
 
     s
 }
 
+/* 
+    AlphaBeta notation's primary downside is that the actual value must be calculated
+    for almost all comparisons.  The time spent on converting 30 rows (over a billion terms)
+    is not signifcant, especially when compared to the time spent generating rows.
+*/
 fn min_max(s: &Sequence, alpha: f64, beta: f64) {
     let mut min = s[0];
     let mut max = s[0];
@@ -128,6 +159,11 @@ fn min_max(s: &Sequence, alpha: f64, beta: f64) {
     println!("\nMinimum:\n\t{}", min);
 }
 
+/* 
+    Since AlphaBeta is stored as two u32 variables for RAM usage, the sums cannot be done
+    on single term since the 32-bit unsigned integer limit is hit far too quickly.  Instead,
+    we do the addition separately on two 64-bit unsigned integers.
+*/
 fn sum(s: &Sequence, alpha: f64, beta: f64) -> f64 {
     let mut alpha_sum: u64 = 0;
     let mut beta_sum: u64 = 0;
@@ -135,21 +171,27 @@ fn sum(s: &Sequence, alpha: f64, beta: f64) -> f64 {
     for num in s {
         alpha_sum += num.alpha as u64;
         beta_sum += num.beta as u64;
-        // println!("{}", sum);
     };
     
     (alpha_sum as f64 * alpha) + (beta_sum as f64 * beta)
 }
 
+/* 
+    Mean is just a basic combination of the sum function and the length of the vector.
+*/
 fn mean(s: &Sequence, alpha: f64, beta: f64) {
     let count = s.len() as f64;
-
     let sum = sum(s, alpha, beta);
 
     println!("\nMean:");
     println!("\t{}", sum / count);
 }
 
+/* 
+    Since this requries comparisons, each term must be converted to a float.  After that,
+    it essentially is just rolling through the vector and logs the indexes for the given
+    number to find.
+*/
 fn find_elem_index(s: &Sequence, n: f64, alpha: f64, beta: f64) {
     let mut results = Vec::new();
     for (index, elem) in s.iter().enumerate() {
@@ -172,6 +214,13 @@ fn find_elem_index(s: &Sequence, n: f64, alpha: f64, beta: f64) {
     println!("")
 }
 
+/* 
+    This program was originally written in Python, ported to Rust for speed, so the
+    row generator was never ported since it ran fast enough as is and my understanding
+    of Rust at the time was not good enough.  One improvement would be either porting
+    rows.py to Rust or using PyO3 to call the functions without being forced to pass
+    the sequence data via a file.
+*/
 fn row_generator(mut s: Sequence) {
     let mut file = File::create("sequence.txt").unwrap();
     let mut contents = format!("{}", s.remove(0));
--- src/sequence.rs
+++ src/sequence.rs
@@ -9,29 +9,54 @@ pub struct StandaloneSequence {
     term: AlphaBeta,
     alpha_value: f64,
     beta_value: f64,
+
     cur_term: u32,
     limit: u32,
-    max: f64,
-    min: f64
 }
 
+/* 
+    Okay, we'll have the Standalone conversation here.  This is something I whipped up
+    in a few days based on my desire to not rent out AWS space to calculate some numbers.
+    Since faking infinity computationally requires a very large number (and row formatting
+    is very useful with this sequence), the amount of RAM necessary to guesstimate what
+    heading off to infinity is obscene (AWS only offers up to 4TB of RAM, which would
+    end up generating 42 rows).
+
+    To combat this, what if computations such as max and min were guesstimated used
+    on-the-fly indexing instead of storing each number?  Since there is no non-recursive
+    formula (yet) for calculating a given value knowing just alpha, beta, and the index,
+    each number has to be calculated recursively,  Because of the how the sequence works,
+    this triples the computation time for each additional row, compared to double the
+    current primary method uses.
+
+    "Why keep this here?" Becuase if more rows are neded, tripling computation time can
+    be mitigated using parrallel computations.  Running this in a swarm of AWS nodes that
+    total out to be 1024 CPU cores would drastically reduce computation time since each
+    instance could store the previous 42 rows in memory and compute the next ones on the
+    fly, communicating the results to a master node.
+*/
 pub fn new_standalone(alpha: f64, beta: f64, size: u32) -> StandaloneSequence {
     StandaloneSequence {
         term: AlphaBeta {
             alpha: 0,
             beta: 0
         },
+
         alpha_value: alpha,
         beta_value: beta,
+
         cur_term: 0,
         limit: size,
-        max: alpha.max(beta),
-        min: alpha.min(beta)
     }
 }
 
 impl StandaloneSequence {
+    /* 
+        Recursive function used for on-the-fly term computation.  Calculates what the term
+        at the given index should be, as an AlphaBeta, and then returns it
+    */
     pub fn index(&self, index: u32) -> AlphaBeta {
+        // Base values
         if index == 1  {
             return AlphaBeta{
                 alpha: 1,
@@ -44,13 +69,19 @@ impl StandaloneSequence {
             }
         }
 
+        // Index is even
         if index % 2 == 0 {
-            self.index(index / 2) + self.index(index / 2 + 1)
-        } else {
-            self.index((index - 1) / 2)
+            return self.index(index / 2) + self.index(index / 2 + 1)
         }
+        
+        // Index is odd
+        self.index((index - 1) / 2)
     }
 
+    /* 
+        Generates the next term in the sequence as a float, stopping at the limit.
+        This could probably be parallelised
+    */
     pub fn next(&mut self) -> Option<f64> {
         if self.cur_term == self.limit {
             return None
@@ -65,6 +96,9 @@ impl StandaloneSequence {
         Some(alpha + beta)
     }
 
+    /* 
+
+    */
     pub fn sum(&self) -> f64 {
         let mut s = self.clone();
         let mut sum = 0.0;
@@ -83,12 +117,16 @@ impl StandaloneSequence {
         self.sum() / self.limit as f64
     }
 
+    // TODO
     pub fn min(&self) -> f64 {
-        self.min
+        println!("Not yet implemented");
+        0.0
     }
 
+    // TODO
     pub fn max(&self) -> f64 {
-        self.max
+        println!("Not yet implemented");
+        0.0
     }
 }
 
@@ -105,6 +143,7 @@ impl AlphaBeta {
     }
 }
 
+// Used for row generation
 impl fmt::Display for AlphaBeta {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}a+{}b", self.alpha, self.beta)
